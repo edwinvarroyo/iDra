f108b0c9cf83dd04f0bc8db889659697d9143d01
MitchelHdz <sebastianvicky_@hotmail.com>
2015-11-14 07:39:09 -0600

    adding gitignore

---
+++ public/javascript/init.js
@@ -0,0 +1,11 @@
+$(".meter > span").each(function() {
+  $(this)
+    .data("origWidth", $(this).width())
+    .width(0)
+    .animate({
+      width: $(this).data("origWidth") // or + "%" if fluid
+    }, 1200);
+});
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
